a30f782350480a8c3b1e6031aa5512d72483e2b9
VaradK62442 <varadkulk123@gmail.com>
2025-05-18 16:02:11 +0100

    remove content

--- src/components/content/About.js
+++ src/components/content/About.js
@@ -1,7 +1,7 @@
 function About() {
     return (
         <div>
-            <p>About me</p>
+        
         </div>
     );
 }
--- src/components/content/Projects.js
+++ src/components/content/Projects.js
@@ -1,7 +1,7 @@
 function Projects() {
     return (
         <div>
-            <p>My projects</p>
+        
         </div>
     );
 }
