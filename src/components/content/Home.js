a69ec653bd0b7eb0e39f04eac2c0832f7de3eb45
VaradK62442 <varadkulk123@gmail.com>
2025-05-18 17:13:38 +0100

    tweaks to home

--- src/components/content/Home.js
+++ src/components/content/Home.js
@@ -1,19 +1,18 @@
 function Home() {
     return (
         <div>
-            <p>Hi, I'm Varad Kulkarni, currently an undergraduate student at the University of Glasgow.</p>
-            <p>This website is a personal portfolio to show off things I've done, things I'm working on, and generally things I'm passionate about!</p>
+            <p>Hi, I'm Varad Kulkarni, currently an undergraduate student at the University of Glasgow, studying Computer Science and Mathematics.</p>
+            <p>This website is a personal portfolio to show off things I've done, things I'm working on, and generally things that I'm passionate about!</p>
             <br />
-            <p>Each section takes you through something about me:</p>
-            <ul class="list-disc pl-5">
+            <ul className="list-disc pl-5">
                 <li>
-                    About: A little bit about me, my background, and my interests.
+                    <span class="font-bold">About:</span> A little bit about me, my background, and my interests.
                 </li>
                 <li>
-                    Experience: A summary of my experiences.
+                    <span class="font-bold">Experience:</span> A brief summary of my experience.
                 </li>
                 <li>
-                    Projects: A showcase of my projects.
+                    <span class="font-bold">Projects:</span> A thorough showcase of my projects.
                 </li>
             </ul>
         </div>
