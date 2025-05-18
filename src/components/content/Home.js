beaeaf2cdc664beb1cd5ac1b34aeff167d2a3c5e
VaradK62442 <varadkulk123@gmail.com>
2025-05-18 16:30:58 +0100

    fill in content a little

--- src/components/content/Home.js
+++ src/components/content/Home.js
@@ -1,7 +1,21 @@
 function Home() {
     return (
         <div>
-        
+            <p>Hi, I'm Varad Kulkarni, currently an undergraduate student at the University of Glasgow.</p>
+            <p>This website is a personal portfolio to show off things I've done, things I'm working on, and generally things I'm passionate about!</p>
+            <br />
+            <p>Each section takes you through something about me:</p>
+            <ul class="list-disc pl-5">
+                <li>
+                    About: A little bit about me, my background, and my interests.
+                </li>
+                <li>
+                    Experience: A summary of my experiences.
+                </li>
+                <li>
+                    Projects: A showcase of my projects.
+                </li>
+            </ul>
         </div>
     );
 }
