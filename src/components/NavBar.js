37e241a7ca6f65ba39f1265345927494ccaa5366
VaradK62442 <varadkulk123@gmail.com>
2025-05-18 22:31:28 +0100

    load parts about other pages on home page dynamically, and make them links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,10 +2,10 @@ import { NavLink } from "react-router-dom";
 
 const routes = [
     { path: "/", name: "Home" },
-    { path: "/about", name: "About" },
-    { path: "/education", name: "Education" },
-    { path: "/experience", name: "Experience" },
-    { path: "/projects", name: "Projects" },
+    { path: "/about", name: "About", description: "A little bit about me, my background, and my interests." },
+    { path: "/education", name: "Education", description: "An overview of my education." },
+    { path: "/experience", name: "Experience", description: "A brief summary of my experience." },
+    { path: "/projects", name: "Projects", description: "A thorough showcase of my projects." },
 ]
 
 function NavBar() {
@@ -34,3 +34,4 @@ function NavBar() {
 }
 
 export default NavBar;
+export { routes };
--- src/components/content/Home.js
+++ src/components/content/Home.js
@@ -1,3 +1,6 @@
+import { NavLink } from "react-router-dom";
+import { routes } from "../NavBar";
+
 function Home() {
     return (
         <div>
@@ -5,15 +8,16 @@ function Home() {
             <p>This website is a personal portfolio to show off things I've done, things I'm working on, and generally things that I'm passionate about!</p>
             <br />
             <ul className="list-disc pl-5">
-                <li>
-                    <span class="font-bold">About:</span> A little bit about me, my background, and my interests.
-                </li>
-                <li>
-                    <span class="font-bold">Experience:</span> A brief summary of my experience.
-                </li>
-                <li>
-                    <span class="font-bold">Projects:</span> A thorough showcase of my projects.
-                </li>
+                {routes
+                    .filter((route) => route.description)
+                    .map((route) => (
+                        <li key={route.path}>
+                            <NavLink to={route.path}>
+                                <span class="font-bold">{route.name}</span>
+                            </NavLink>
+                            : {route.description}
+                        </li>
+                ))}
             </ul>
         </div>
     );
