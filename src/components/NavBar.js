22dc04a1abecc2ad28e83b3bf5327faf4c8761ba
VaradK62442 <varadkulk123@gmail.com>
2025-05-18 22:21:03 +0100

    make nav slightly darker blue

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,13 +3,14 @@ import { NavLink } from "react-router-dom";
 const routes = [
     { path: "/", name: "Home" },
     { path: "/about", name: "About" },
-    { path: "/experiences", name: "Experiences" },
+    { path: "/education", name: "Education" },
+    { path: "/experience", name: "Experience" },
     { path: "/projects", name: "Projects" },
 ]
 
 function NavBar() {
     return (
-        <nav class="bg-blue text-white py-2 shadow">
+        <nav class="bg-blue-400 text-white py-2 shadow">
             <ul class="flex space-x-4 justify-center">
                 {routes.map((route) => (
                         <li key={route.path} class="hover:text-crimson">
