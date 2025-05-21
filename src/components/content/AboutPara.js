f2ccfd199a950c3c4072278fbb4d555c5cc83af9
VaradK62442 <varadkulk123@gmail.com>
2025-05-21 13:14:09 +0100

    make about paragraph component with image and paragraph; fill in about page more

---
+++ src/components/content/AboutPara.js
@@ -0,0 +1,20 @@
+function AboutPara({ paragraph, image, position }) {
+    return (
+        <div
+            class={`flex items-center mb-4 ${
+                position === "left" ? "flex-row" : "flex-row-reverse"
+            }`}
+        >
+            <img
+                src={"/media/" + image}
+                alt="About"
+                class="w-40 h-40 object-cover rounded-xl shadow-lg mx-4"
+            />
+            <p class="text-justify">
+                {paragraph}
+            </p>
+        </div>
+    );
+}
+
+export default AboutPara;
