986068f0e3cff75d9df6f96343a12ed60c9f7835
VaradK62442 <varadkulk123@gmail.com>
2025-05-21 13:14:18 +0100

    add image to home using the about para component

--- src/components/content/Home.js
+++ src/components/content/Home.js
@@ -1,11 +1,18 @@
 import { NavLink } from "react-router-dom";
+import AboutPara from "./AboutPara";
 import { routes } from "../config";
 
 function Home() {
     return (
         <div>
-            <p>Hi, I'm Varad Kulkarni, currently an undergraduate student at the University of Glasgow, studying Computer Science and Mathematics.</p>
-            <p>This website is a personal portfolio to show off things I've done, things I'm working on, and generally things that I'm passionate about!</p>
+            <AboutPara
+                paragraph = {
+                    "Hi, I'm Varad Kulkarni, currently an undergraduate student at the University of Glasgow, studying Computer Science and Mathematics.\n" +
+                    "This website is a personal portfolio to show off things I've done, things I'm working on, and generally things that I'm passionate about!\n"
+                }
+                image = "pfp.JPG"
+                position = "left"
+            />
             <br />
             <ul className="list-disc pl-5">
                 {routes
