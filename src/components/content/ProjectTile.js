9b2aedb729848afd201d7580ac079a1f6ca85935
VaradK62442 <varadkulk123@gmail.com>
2025-08-31 12:27:22 +0100

    change div to button for clickability

--- src/components/content/ProjectTile.js
+++ src/components/content/ProjectTile.js
@@ -1,12 +1,12 @@
 function ProjectTile({ project, onClick }) {
     return (
-        <div
+        <button
             className="p-4 bg-crimson rounded-lg shadow-md hover:shadow-lg hover:bg-crimson-600 transition cursor-pointer aspect-square flex flex-col justify-center items-center"
             onClick={() => onClick(project)}
         >
             <h3 className="text-xl font-bold text-center">{project.title}</h3>
             <p className="text-center">{project.shortDescription}</p>
-        </div>
+        </button>
     );
 }
 
